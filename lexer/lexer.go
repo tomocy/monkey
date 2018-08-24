ad24ecd9a3e0c047741334559e5ecd41cd5f1b39
tomocy <tomocy.dev@gmail.com>
2018-08-24 22:34:57 +0900

    add several tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -20,11 +20,38 @@ func New(input string) *Lexer {
 
 func (l *Lexer) NextToken() token.Token {
 	var t token.Token
+	l.skipWhitespace()
 	switch l.char {
 	case '=':
-		t = newToken(token.ASSIGN, l.char)
+		if l.peekChar() == '=' {
+			prevChar := l.char
+			l.readChar()
+			t.Type = token.EQ
+			t.Literal = string(prevChar) + string(l.char)
+		} else {
+			t = newToken(token.ASSIGN, l.char)
+		}
 	case '+':
 		t = newToken(token.PLUS, l.char)
+	case '-':
+		t = newToken(token.MINUS, l.char)
+	case '*':
+		t = newToken(token.ASTERRISK, l.char)
+	case '/':
+		t = newToken(token.SLASH, l.char)
+	case '!':
+		if l.peekChar() == '=' {
+			prevChar := l.char
+			l.readChar()
+			t.Type = token.NOT_EQ
+			t.Literal = string(prevChar) + string(l.char)
+		} else {
+			t = newToken(token.BANG, l.char)
+		}
+	case '<':
+		t = newToken(token.LT, l.char)
+	case '>':
+		t = newToken(token.GT, l.char)
 	case ',':
 		t = newToken(token.COMMA, l.char)
 	case ';':
@@ -40,10 +67,41 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		t.Type = token.EOF
 		t.Literal = ""
+	default:
+		if isLetter(l.char) {
+			t.Literal = l.readIdentifier()
+			t.Type = token.LookUpIdentifier(t.Literal)
+			return t
+		}
+
+		if isDigit(l.char) {
+			t.Literal = l.readNumber()
+			t.Type = token.INT
+			return t
+		}
+
+		t = newToken(token.ILLEGAL, l.char)
 	}
 
 	l.readChar()
 	return t
+}
+
+func (l *Lexer) skipWhitespace() {
+	for isWhitespace(l.char) {
+		l.readChar()
+	}
+}
+
+func isWhitespace(char byte) bool {
+	return char == ' ' || char == '\t' || char == '\n' || char == '\r'
+}
+
+func (l *Lexer) peekChar() byte {
+	if len(l.input) <= l.readingPosition {
+		return 0
+	}
+	return l.input[l.readingPosition]
 }
 
 func newToken(tokenType token.TokenType, char byte) token.Token {
@@ -51,6 +109,32 @@ func newToken(tokenType token.TokenType, char byte) token.Token {
 		Type:    tokenType,
 		Literal: string(char),
 	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	beginPosition := l.position
+	for isLetter(l.char) {
+		l.readChar()
+	}
+
+	return l.input[beginPosition:l.position]
+}
+
+func isLetter(char byte) bool {
+	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_'
+}
+
+func (l *Lexer) readNumber() string {
+	beginPosition := l.position
+	for isDigit(l.char) {
+		l.readChar()
+	}
+
+	return l.input[beginPosition:l.position]
+}
+
+func isDigit(char byte) bool {
+	return '0' <= char && char <= '9'
 }
 
 func (l *Lexer) readChar() {
--- token/token.go
+++ token/token.go
@@ -7,8 +7,18 @@ const (
 	IDENT = "IDENT"
 	INT   = "INT"
 
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN    = "="
+	PLUS      = "+"
+	MINUS     = "-"
+	ASTERRISK = "*"
+	SLASH     = "/"
+	BANG      = "!"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	LT = "<"
+	GT = ">"
 
 	COMMA     = ","
 	SEMICOLON = ";"
@@ -20,11 +30,34 @@ const (
 
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	IF       = "if"
+	ELSE     = "else"
+	RETURN   = "return"
+	TRUE     = "true"
+	FALSE    = "false"
 )
+
+var keywordTypes = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
+}
 
 type TokenType string
 
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+func LookUpIdentifier(ident string) TokenType {
+	if keywordType, ok := keywordTypes[ident]; ok {
+		return keywordType
+	}
+
+	return IDENT
 }
