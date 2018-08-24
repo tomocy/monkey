2c1505fae544fa7d45a26ffcfc6f2bce76eff227
tomocy <tomocy.dev@gmail.com>
2018-08-24 16:42:34 +0900

    add several TokenType

--- token/token.go
+++ token/token.go
@@ -1,5 +1,27 @@
 package token
 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
 type TokenType string
 
 type Token struct {
