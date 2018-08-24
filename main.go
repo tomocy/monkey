bb380c8a82031bd7f1686483207240303ef501d9
tomocy <tomocy.dev@gmail.com>
2018-08-24 23:59:48 +0900

    add helper function to say hello to user

--- main.go
+++ main.go
@@ -10,11 +10,15 @@ import (
 )
 
 func main() {
+	sayHelloToUser()
+	repl.Start(os.Stdin, os.Stdout)
+}
+
+func sayHelloToUser() {
 	user, err := user.Current()
 	if err != nil {
 		log.Fatalln(err)
 	}
 	fmt.Printf("Hello %s! This is the Monkey programming language!", user.Username)
 	fmt.Println("Feel free to type in commands")
-	repl.Start(os.Stdin, os.Stdout)
 }
