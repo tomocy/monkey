046765a2a3aad3e5d578fb564ca07a80f7eae76c
tomocy <tomocy.dev@gmail.com>
2018-08-24 22:43:30 +0900

    add repl

---
+++ repl/repl.go
@@ -0,0 +1,26 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+
+	"io"
+
+	"github.com/tomocy/monkey/lexer"
+	"github.com/tomocy/monkey/token"
+)
+
+const prompt = ">> "
+
+func Start(in io.Reader, w io.Writer) {
+	fmt.Print(prompt)
+	scanner := bufio.NewScanner(in)
+	for scanner.Scan() {
+		sourceCode := scanner.Text()
+		lexer := lexer.New(sourceCode)
+		for t := lexer.NextToken(); t.Type != token.EOF; t = lexer.NextToken() {
+			fmt.Println(t)
+		}
+		fmt.Print(prompt)
+	}
+}
