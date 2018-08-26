c419a0bbba74c2f946c6d94683b446354808bb3a
tomocy <tomocy.dev@gmail.com>
2018-08-26 14:18:33 -0600

    add function to Node to output it as human can read it easily

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,12 @@
 package ast
 
-import "github.com/tomocy/monkey/token"
+import (
+	"github.com/tomocy/monkey/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -28,6 +31,15 @@ func (p Program) TokenLiteral() string {
 	return ""
 }
 
+func (p Program) String() string {
+	b := make([]byte, 0, 10)
+	for _, stmt := range p.Statements {
+		b = append(b, stmt.String()...)
+	}
+
+	return string(b)
+}
+
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
@@ -39,6 +51,19 @@ func (s LetStatement) statement() {
 
 func (s LetStatement) TokenLiteral() string {
 	return s.Token.Literal
+}
+
+func (s LetStatement) String() string {
+	b := make([]byte, 0, 10)
+	b = append(b, s.TokenLiteral()...)
+	b = append(b, ' ')
+	b = append(b, s.Name.String()...)
+	if s.Value != nil {
+		b = append(b, s.Value.String()...)
+	}
+	b = append(b, ';')
+
+	return string(b)
 }
 
 type Identifier struct {
@@ -53,6 +78,10 @@ func (i Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i Identifier) String() string {
+	return i.Value
+}
+
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
@@ -65,6 +94,18 @@ func (s ReturnStatement) TokenLiteral() string {
 	return s.Token.Literal
 }
 
+func (s ReturnStatement) String() string {
+	b := make([]byte, 0, 10)
+	b = append(b, s.TokenLiteral()...)
+	b = append(b, ' ')
+	if s.ReturnValue != nil {
+		b = append(b, s.ReturnValue.String()...)
+	}
+	b = append(b, ';')
+
+	return string(b)
+}
+
 type ExpressionStatement struct {
 	Token      token.Token
 	Expression Expression
@@ -75,4 +116,12 @@ func (s ExpressionStatement) statement() {
 
 func (s ExpressionStatement) TokenLiteral() string {
 	return s.Token.Literal
+}
+
+func (s ExpressionStatement) String() string {
+	if s.Expression != nil {
+		return s.Expression.String()
+	}
+
+	return ""
 }
