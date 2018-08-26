dad4465b4c25f0200e0deb29af5ba30e78195474
tomocy <tomocy.dev@gmail.com>
2018-08-26 13:20:48 -0600

    implement parser.ParseProgram()

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"github.com/tomocy/monkey/ast"
 	"github.com/tomocy/monkey/lexer"
 	"github.com/tomocy/monkey/token"
 )
@@ -24,4 +25,62 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
 	p.currentToken = p.peekToken
 	p.peekToken = p.lexer.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{
+		Statements: make([]ast.Statement, 0),
+	}
+	for p.currentToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currentToken.Type {
+	case token.Let:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() ast.Statement {
+	stmt := &ast.LetStatement{
+		Token: p.currentToken,
+	}
+
+	if !p.isPeekToken(token.Ident) {
+		return nil
+	}
+	p.nextToken()
+
+	stmt.Name = &ast.Identifier{
+		Token: p.currentToken,
+		Value: p.currentToken.Literal,
+	}
+
+	if !p.isPeekToken(token.Assign) {
+		return nil
+	}
+
+	for !p.isCurrentToken(token.Semicolon) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p Parser) isPeekToken(tokenType token.TokenType) bool {
+	return p.peekToken.Type == tokenType
+}
+
+func (p Parser) isCurrentToken(tokenType token.TokenType) bool {
+	return p.currentToken.Type == tokenType
 }
