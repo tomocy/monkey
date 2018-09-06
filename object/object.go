70a66b46f41e844141ebace633aaaacd2e217acb
tomocy <tomocy.dev@gmail.com>
2018-09-06 00:55:45 -1000

    add type for integer object and make it implement Object

--- object/object.go
+++ object/object.go
@@ -1,8 +1,26 @@
 package object
 
+import "fmt"
+
+const (
+	integer = "Integer"
+)
+
 type ObjectType string
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i Integer) Type() ObjectType {
+	return integer
+}
+
+func (i Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
 }
