4b5c4a5bbdfbdd934a8879d9d767fc72d4a43e8a
tomocy <tomocy.dev@gmail.com>
2018-09-06 01:15:56 -1000

    enable it to eval integer

---
+++ evaluator/evaluator.go
@@ -0,0 +1,28 @@
+package evaluator
+
+import (
+	"github.com/tomocy/monkey/ast"
+	"github.com/tomocy/monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Value)
+	case *ast.Integer:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var obj object.Object
+	for _, stmt := range stmts {
+		obj = Eval(stmt)
+	}
+
+	return obj
+}
