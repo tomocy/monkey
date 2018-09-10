710e9cd14697db2de9f6630fb2b10cb873587923
tomocy <tomocy.dev@gmail.com>
2018-09-09 18:34:40 -1000

    make evaluator eval bang

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -17,6 +17,8 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Value)
+	case *ast.Prefix:
+		return evalPrefix(node.Operator, node.RightValue)
 	case *ast.Integer:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
@@ -33,6 +35,27 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return obj
+}
+
+func evalPrefix(operator string, exp ast.Expression) object.Object {
+	rightVal := Eval(exp)
+	switch operator {
+	case "!":
+		return evalBang(rightVal)
+	default:
+		return nullObj
+	}
+}
+
+func evalBang(rightVal object.Object) object.Object {
+	switch rightVal {
+	case trueObj:
+		return falseObj
+	case falseObj, nullObj:
+		return trueObj
+	default:
+		return falseObj
+	}
 }
 
 func convertToBooleanObject(b bool) object.Object {
