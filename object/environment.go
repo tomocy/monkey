72ca5322523cc0aef29d7e53f826db7e092a1479
tomocy <tomocy.dev@gmail.com>
2018-09-15 22:54:46 +0900

    add and fix functions for enclosed environment

--- object/environment.go
+++ object/environment.go
@@ -1,7 +1,8 @@
 package object
 
 type Environment struct {
-	objs map[string]Object
+	objs  map[string]Object
+	outer *Environment
 }
 
 func NewEnvironment() *Environment {
@@ -10,8 +11,19 @@ func NewEnvironment() *Environment {
 	}
 }
 
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+
+	return env
+}
+
 func (e Environment) Get(name string) (Object, bool) {
 	obj, ok := e.objs[name]
+	if !ok && e.outer != nil {
+		return e.outer.Get(name)
+	}
+
 	return obj, ok
 }
 
