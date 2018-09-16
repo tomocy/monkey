6369045e6d2519ac76a3257536dcb82ee6ac796a
tomocy <tomocy.dev@gmail.com>
2018-09-16 18:45:29 +0900

    make evaluator evaluate len

--- evaluator/builtin_functions.go
+++ evaluator/builtin_functions.go
@@ -5,7 +5,16 @@ import "github.com/tomocy/monkey/object"
 var builtinFns = map[string]*object.BuiltinFunctionObject{
 	"len": &object.BuiltinFunctionObject{
 		Function: func(objs ...object.Object) object.Object {
-			return nullObj
+			if len(objs) != 1 {
+				return newError("too many arguments to len: expected 1, but got %d", len(objs))
+			}
+			obj := objs[0]
+			str, ok := obj.(*object.StringObject)
+			if !ok {
+				return newError("unknown operation: len(%s)", obj.Type())
+			}
+
+			return &object.IntegerObject{Value: int64(len(str.Value))}
 		},
 	},
 }
