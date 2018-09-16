7c2672666a5b46042196752e37bbad75bfc2a1d7
tomocy <tomocy.dev@gmail.com>
2018-09-16 18:30:30 +0900

    fix

--- evaluator/builtin_functions.go
+++ evaluator/builtin_functions.go
@@ -2,8 +2,10 @@ package evaluator
 
 import "github.com/tomocy/monkey/object"
 
-var builtinFns = map[string]func(objs ...object.Object) object.Object{
-	"len": func(objs ...object.Object) object.Object {
-		return nullObj
+var builtinFns = map[string]*object.BuiltinFunctionObject{
+	"len": &object.BuiltinFunctionObject{
+		Function: func(objs ...object.Object) object.Object {
+			return nullObj
+		},
 	},
 }
