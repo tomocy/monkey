22bbe18bb26a282ea0299e1b2ab7ddf6f2b601b4
tomocy <tomocy.dev@gmail.com>
2018-09-17 04:39:09 +0900

    make evaluator evalute len(Array)

--- evaluator/builtin_functions.go
+++ evaluator/builtin_functions.go
@@ -8,13 +8,14 @@ var builtinFns = map[string]*object.BuiltinFunctionObject{
 			if len(objs) != 1 {
 				return newError("too many arguments to len: expected 1, but got %d", len(objs))
 			}
-			obj := objs[0]
-			str, ok := obj.(*object.StringObject)
-			if !ok {
+			switch obj := objs[0].(type) {
+			case *object.StringObject:
+				return &object.IntegerObject{Value: int64(len(obj.Value))}
+			case *object.ArrayObject:
+				return &object.IntegerObject{Value: int64(len(obj.Elements))}
+			default:
 				return newError("unknown operation: len(%s)", obj.Type())
 			}
-
-			return &object.IntegerObject{Value: int64(len(str.Value))}
 		},
 	},
 }
