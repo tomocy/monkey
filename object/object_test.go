618dd045b1ddb15d0b714df445334d61c5138600
tomocy <tomocy.dev@gmail.com>
2018-09-18 23:40:26 +0900

    fix test for hash key

--- object/object_test.go
+++ object/object_test.go
@@ -4,17 +4,18 @@ import "testing"
 
 func TestHashKey(t *testing.T) {
 	type expect struct {
-		obj1 Object
-		obj2 Object
+		name string
+		obj1 HashKeyable
+		obj2 HashKeyable
 	}
 	expects := []expect{
-		{&IntegerObject{Value: 5}, &IntegerObject{Value: 1}},
-		{&BooleanObject{Value: true}, &BooleanObject{Value: true}},
-		{&StringObject{Value: "hello world"}, &StringObject{Value: "hello world"}},
+		{"Integer", &IntegerObject{Value: 5}, &IntegerObject{Value: 5}},
+		{"Boolean", &BooleanObject{Value: true}, &BooleanObject{Value: true}},
+		{"String", &StringObject{Value: "hello world"}, &StringObject{Value: "hello world"}},
 	}
 	diffObj := &StringObject{Value: "Different"}
 	for _, expect := range expects {
-		t.Run(expect.obj1.Inspect(), func(t *testing.T) {
+		t.Run(expect.name, func(t *testing.T) {
 			if expect.obj1.HashKey() != expect.obj2.HashKey() {
 				t.Error("objects with same contents have different hash keys")
 			}
