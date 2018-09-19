1ad14777defc5a5ea2656479258cb44c12e6029c
tomocy <tomocy.dev@gmail.com>
2018-09-20 00:04:30 +0900

    make it possible to expand macros

--- evaluator/macro.go
+++ evaluator/macro.go
@@ -57,3 +57,66 @@ func isMacroDefinition(stmt ast.Statement) bool {
 	_, ok = letStmt.Value.(*ast.Macro)
 	return ok
 }
+
+func ExpandMacros(program *ast.Program, env *object.Environment) ast.Node {
+	return ast.Modify(program, func(node ast.Node) ast.Node {
+		funcCall, ok := node.(*ast.FunctionCall)
+		if !ok {
+			return node
+		}
+
+		macro, ok := getMacroObject(funcCall, *env)
+		if !ok {
+			return node
+		}
+
+		quotedArgObjs := quoteExpressions(funcCall.Arguments)
+
+		return applyMacro(*macro, quotedArgObjs)
+	})
+}
+
+func getMacroObject(node *ast.FunctionCall, env object.Environment) (*object.MacroObject, bool) {
+	ident, ok := node.Function.(*ast.Identifier)
+	if !ok {
+		return nil, false
+	}
+
+	obj, ok := env.Get(ident.Value)
+	if !ok {
+		return nil, false
+	}
+
+	macro, ok := obj.(*object.MacroObject)
+
+	return macro, ok
+}
+
+func quoteExpressions(nodes []ast.Expression) []*object.QuoteObject {
+	exps := make([]*object.QuoteObject, len(nodes))
+	for i, exp := range nodes {
+		exps[i] = &object.QuoteObject{Value: exp}
+	}
+
+	return exps
+}
+
+func applyMacro(macro object.MacroObject, argObjs []*object.QuoteObject) ast.Node {
+	extendedEnv := extendMacroEnvironment(macro, argObjs)
+	obj := Eval(macro.Body, extendedEnv)
+	quote, ok := obj.(*object.QuoteObject)
+	if !ok {
+		panic("invalid macro definition: macro should return quoted value")
+	}
+
+	return quote.Value
+}
+
+func extendMacroEnvironment(macro object.MacroObject, argObjs []*object.QuoteObject) *object.Environment {
+	extendedEnv := object.NewEnclosedEnvironment(macro.Env)
+	for i, param := range macro.Parameters {
+		extendedEnv.Set(param.Value, argObjs[i])
+	}
+
+	return extendedEnv
+}
