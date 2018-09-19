5c153e1dd9ef38325ccc9705a026849bf6b64c89
tomocy <tomocy.dev@gmail.com>
2018-09-19 15:08:12 +0900

    add test for unquote

--- evaluator/macro_test.go
+++ evaluator/macro_test.go
@@ -37,3 +37,32 @@ func TestQuote(t *testing.T) {
 		})
 	}
 }
+
+func TestUnquote(t *testing.T) {
+	tests := []struct {
+		in     string
+		expect string
+	}{
+		{"quote(unquote(5))", "5"},
+		{"quote(unquote(5 + 5))", "10"},
+		{"quote(5 + unquote(2 + 3))", "(5 + 5)"},
+	}
+	for _, test := range tests {
+		t.Run(test.in, func(t *testing.T) {
+			parser := parser.New(lexer.New(test.in))
+			program := parser.ParseProgram()
+			env := object.NewEnvironment()
+			got := Eval(program, env)
+			quote, ok := got.(*object.QuoteObject)
+			if !ok {
+				t.Fatalf("assertion faild: expected *object.QuoteObject, but got %T\n", got)
+			}
+			if quote.Value == nil {
+				t.Fatal("quote.Value was nil")
+			}
+			if quote.Value.String() != test.expect {
+				t.Errorf("quote.Value.String() returned wrong value: expected %s, but got %s\n", test.expect, quote.Value.String())
+			}
+		})
+	}
+}
