1cd4fa51ff1c90c826cda13d9e0eeabe44d01bb2
tomocy <tomocy.dev@gmail.com>
2018-09-19 22:45:00 +0900

    add test for evaluating macro

--- evaluator/macro_test.go
+++ evaluator/macro_test.go
@@ -70,3 +70,49 @@ func TestUnquote(t *testing.T) {
 		})
 	}
 }
+
+func TestDefineMacro(t *testing.T) {
+	in := `
+	let num = 1;
+	let func = fn(x, y) { return x + y; };
+	let m = macro(x, y) { return x + y; };
+	`
+	parser := parser.New(lexer.New(in))
+	program := parser.ParseProgram()
+	env := object.NewEnvironment()
+
+	DefineMacro(program, env)
+
+	if len(program.Statements) != 2 {
+		t.Fatalf("len(program.Statements) returned wrong value: expected 2, but got %d\n", len(program.Statements))
+	}
+
+	if _, ok := env.Get("num"); ok {
+		t.Error("invalid environment: num shuld not be defined")
+	}
+	if _, ok := env.Get("func"); ok {
+		t.Error("invalid environment: func should not be defined")
+	}
+
+	obj, ok := env.Get("m")
+	if !ok {
+		t.Fatal("invalid environment: m should be defined")
+	}
+
+	macro, ok := obj.(*object.MacroObject)
+	if !ok {
+		t.Fatalf("assertion faild: expected *object.MacroObject, but got %T\n", obj)
+	}
+	if len(macro.Parameters) != 2 {
+		t.Fatalf("len(macro.Parameters) returned wrong value: expected 2, but got %d\n", len(macro.Parameters))
+	}
+	if macro.Parameters[0].String() != "x" {
+		t.Errorf("macro.Parameters[0].String() returend wrong value: expected x, but got %s\n", macro.Parameters[0].String())
+	}
+	if macro.Parameters[1].String() != "y" {
+		t.Errorf("macro.Parameters[1].String() returend wrong value: expected y, but got %s\n", macro.Parameters[1].String())
+	}
+	if macro.Body.String() != "(x + y)" {
+		t.Errorf("macro.Body.String() returned wront value: expected (x + y), but got %s\n", macro.Body.String())
+	}
+}
