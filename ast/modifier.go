0f803fc6a58b973d0a1b9e047276238b8af8b99b
tomocy <tomocy.dev@gmail.com>
2018-09-19 16:03:59 +0900

    add function to modify ast

---
+++ ast/modifier.go
@@ -0,0 +1,28 @@
+package ast
+
+type modifier func(Node) Node
+
+func Modify(node Node, modifier modifier) Node {
+	switch node := node.(type) {
+	case *Program:
+		return modifyProgram(node, modifier)
+	case *ExpressionStatement:
+		return modifyExpressionStatement(node, modifier)
+	default:
+		return modifier(node)
+	}
+}
+
+func modifyProgram(node *Program, modifier modifier) Node {
+	for i, stmt := range node.Statements {
+		node.Statements[i], _ = Modify(stmt, modifier).(Statement)
+	}
+
+	return node
+}
+
+func modifyExpressionStatement(node *ExpressionStatement, modifier modifier) Node {
+	node.Value, _ = Modify(node.Value, modifier).(Expression)
+
+	return node
+}
